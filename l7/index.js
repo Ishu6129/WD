2669c2c21a71f16dad216385c13d644dabd4aedd
Ishu6129 <ishuagrawal124356@gmail.com>
2025-01-29 23:39:44 +0530

    l7

--- l7/index.js
+++ l7/index.js
@@ -3,14 +3,19 @@ const express = require('express')
 const app=express()
 const fs=require('fs')
 
-app.set('view engine','ejs')
+
 app.use(express.json())
 app.use(express.urlencoded({extended:true}));
 app.use(express.static(path.join(__dirname,'public')))
 app.set('view engine','ejs')
 
-app.get("/",function(req,res){
-
-    res.render("index")
+app.get('/',function(req,res){
+    fs.readdir(`./files`,(err,files)=>{
+        res.render("index",{files:files});
+    })
+    
+})
+app.post('/create',function(req,res){
+    console.log(req.body);
 })
 app.listen(3000)
