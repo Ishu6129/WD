0b5cafaee5722a5eff3a57b363b015205ba2357c
Ishu6129 <ishuagrawal124356@gmail.com>
2025-01-30 09:15:14 +0530

    updated

--- Projects/createNotes/index.js
+++ Projects/createNotes/index.js
@@ -3,6 +3,8 @@ const express = require('express')
 const app=express()
 const fs=require('fs')
 
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine','ejs')
 
 app.get('/',function(req,res){
