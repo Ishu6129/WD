1e1f740456b638b19992cc47e02dde4ec735d135
Ishu6129 <ishuagrawal124356@gmail.com>
2025-02-01 01:28:11 +0530

    finalized

--- Projects/createNotes/index.js
+++ Projects/createNotes/index.js
@@ -13,14 +13,39 @@ app.get('/',function(req,res){
     })
     
 })
+
 app.post('/create',function(req,res){
     fs.writeFile(`./files/${req.body.title.split(" ").join("")}.txt`,req.body.details,function(err){
         res.redirect('/');
     });
 })
+
 app.get('/file/:filename',function(req,res){
     fs.readFile(`./files/${req.params.filename}`,"utf-8",function(err,filedata){
         res.render("show",{filename:req.params.filename,filedata:filedata});
     });
 })
-app.listen(3000)
+app.get('/delete/:filename', function(req, res) {
+    const filePath = `./files/${req.params.filename}`;
+    fs.unlink(filePath, function(err){
+      if (err) {
+        res.status(500).send('Failed to delete the file');
+      } else {
+        res.redirect('/');
+      }
+    });
+});
+
+app.get('/edit/:filename',function(req,res){
+    res.render('edit',{filename:req.params.filename});
+})
+
+app.post('/edit',function(req,res){
+  fs.rename(`./files/${req.body.previous}.txt`,`./files/${req.body.new}.txt`,(err)=>{
+    res.redirect('/')
+  })
+})  
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+  });
